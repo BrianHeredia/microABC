37d1fa2153d0523835302b58c1e1b55b538f6bbb
unknown <brianheredia@gmail.com>
2019-03-01 15:38:28 -0400

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    BannerComponent,
+    BusquedaComponent
   ],
   imports: [
     BrowserModule,
